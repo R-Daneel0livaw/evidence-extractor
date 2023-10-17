3b48d11955f40e74690b3092a7f14f28cd2484a8
Eric Hine <eric.hine2214@gmail.com>
2023-10-17 19:39:52 -0400

    Brainstorm on how to handle Stats Nodes.

--- R/orchestrator.R
+++ R/orchestrator.R
@@ -18,3 +18,20 @@ players_table <- m_get_player_df()
 colleges_table <- m_get_college_df()
 games_table <- m_get_game_df()
 awards_table <- m_get_awards_df()
+
+
+filtered_df <- 
+  teams_table %>%
+  select((which(names(.) == "to")):last_col())
+
+
+teams_stats <-
+  map2(
+    names(filtered_df),
+    filtered_df,
+    \(name, value, id) data.frame(name, value = as.character(value), id, type = "STAT"),
+    teams_table$id
+  ) %>%
+  bind_rows() 
+
+
