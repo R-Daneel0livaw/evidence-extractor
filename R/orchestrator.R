4bca1c412d516af242e89c8a0fec58159964fc76
erich <eric.hine2214@gmail.com>
2024-11-09 13:06:55 -0500

    Cleanup

--- R/orchestrator.R
+++ R/orchestrator.R
@@ -16,15 +16,15 @@ source("R/utils.R")
 seasons_nodes <- m_get_season_df()
 seasons_stats <- m_get_season_top_stats()
 seasons_teams_stats <- m_get_season_team_stats()
-# 
+
 # teams_nodes <- m_get_team_df()
 # teams_stats <- m_get_team_top_stats()
 # 
 # players_nodes <- m_get_player_df()
 # players_stats <- m_get_player_top_stats()
-# 
+
 # games_nodes <- m_get_game_df()
 # games_players_stats <- m_get_game_player_stats()
-# 
+
 # colleges_table <- m_get_college_df()
 # awards_table <- m_get_awards_df()
