72cda3ebbbcb76e3317b0cc0363ae28fefa9340b
erich <eric.hine2214@gmail.com>
2024-12-16 18:36:09 -0500

    Start on games top stats

--- R/data_prep/game/games.R
+++ R/data_prep/game/games.R
@@ -8,6 +8,34 @@ get_game_df <- function() {
 
 m_get_game_df <- memoise(get_game_df)
 
+get_games_top_stats <- function() {
+  # seasons_stats_page <- discover_page("https://www.basketball-reference.com/leagues/NBA_stats_per_game.html")
+  # seasons_view <- seasons_stats_page("table#stats-Regular-Season")
+  # 
+  # identifier <-
+  #   extract_identifier(view = seasons_view,
+  #                      identifier = "tr td[data-stat='season'] a",
+  #                      name = c("id", "season"),
+  #                      id = str_extract(id, ".*/([A-Z]+_\\d+).html", 1))
+  # 
+  # seasons_identifier_table <-
+  #   join_identifier(initial_table = get_clean_seasons_stats_table(seasons_view),
+  #                   identifier = identifier,
+  #                   season)
+  # 
+  # seasons_stats_table <-
+  #   seasons_identifier_table %>%
+  #   mutate(type = "SEASON") %>%
+  #   relocate(type, id) %>%
+  #   select(-season)
+  # 
+  # seasons_stats <- convert_to_stats(seasons_stats_table, "age")
+  # 
+  # seasons_stats
+}
+
+m_get_games_top_stats <- memoise(get_games_top_stats)
+
 get_games_group <- function(month, season) {
   games_page <- discover_page(paste0("https://www.basketball-reference.com/leagues/NBA_", season,
     "_games-", month, ".html"))
