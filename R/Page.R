10691b882c959fc15d65e8472a45b1a8a887faee
erich <eric.hine2214@gmail.com>
2025-01-20 16:49:46 -0500

    Allow for dynamic URL in OOP approach

--- R/Page.R
+++ R/Page.R
@@ -1,9 +1,16 @@
 Page <- function(config) {
   page <- list(
     config = config,
-    fetch_table = function(identifier) {
-      message("Fetching page: ", config$url)
-      page_content <- read_html(config$url) %>%
+    fetch_table = function(identifier, dynamic_values = list()) {
+      url <- config$url
+      if (!is.null(dynamic_values) && length(dynamic_values) > 0) {
+        for (key in names(dynamic_values)) {
+          placeholder <- paste0("{", key, "}")  
+          url <- gsub(placeholder, dynamic_values[[key]], url)
+        }
+      }
+      message("Fetching page: ", url)
+      page_content <- read_html(url) %>%
         html_element(identifier)
       return(page_content)
     }
