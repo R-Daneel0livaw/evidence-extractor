e6d3ab90a71d2f05782244c68a10aa080e59e864
erich <eric.hine2214@gmail.com>
2025-01-21 18:17:28 -0500

    Add index support to fetch-page

--- R/Page.R
+++ R/Page.R
@@ -1,14 +1,18 @@
 Page <- function(config) {
   page <- list(
     config = config,
-    fetch_table = function(identifier, dynamic_values = list()) {
-      url <- config$url
+    fetch_table = function(identifier, dynamic_values = list(), index = 1) {
+      if (index > nrow(config) || index < 1) {
+        stop("Invalid index: Please provide a valid row index.")
+      }
+      url <- config$url[index]
       if (!is.null(dynamic_values) && length(dynamic_values) > 0) {
         for (key in names(dynamic_values)) {
-          placeholder <- paste0("\\{", key, "\\}")  
+          placeholder <- paste0("\\{", key, "\\}") 
           url <- gsub(placeholder, dynamic_values[[key]], url)
         }
       }
+      
       message("Fetching page: ", url)
       page_content <- read_html(url) %>%
         html_element(identifier)
