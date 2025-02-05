de68ef43fec832c898abe63f503f23bb49185c68
erich <eric.hine2214@gmail.com>
2025-02-05 17:44:29 -0500

    More work towards using the base function for Teams nodes

--- R/TeamsPage.R
+++ R/TeamsPage.R
@@ -6,21 +6,54 @@ TeamsPage <- function(config) {
 get_page_node.TeamsPage <- function(page) {
   # base_get_page_node(
   #   page = page,
-  #   clean_fn = get_clean_seasons_table,  
+  #   clean_fn = get_clean_teams_table,
   #   join_fn = function(view, identifier) {
-  #     join_identifier(view, identifier, season)
+  #     join_teams_identifier(view, identifier)
   #   },
   #   mutate_fn = function(data) {
   #     data %>%
-  #       mutate(
-  #         start = as.numeric(str_replace(season, "-.*", "")),
-  #         end = start + 1,
-  #         type = page$config$type
-  #       )
+  #       join_teams_alternative_names(get_teams_alternative_names(data)) %>% 
+  #       mutate(type = page$config$type) %>%
+  #       relocate(type, id, team, alternative_names) 
   #   },
   #   filter_fn = function(data) {
-  #     data %>% filter(str_detect(id, "NBA"))
+  #     data %>% mutate(current = TRUE)
   #   },
-  #   select_cols = c("type", "id", "season", "start", "end")
+  #   select_cols =  c("-current:level", "-lg")
   # )
+}
+
+get_clean_teams_table <- function(view) {
+  seasons_initial_table <-
+    view %>% 
+    get_clean_table() %>% 
+    rename(team = franchise)
+  
+  seasons_initial_table
+}
+
+join_teams_identifier <- function(view, identifier) {
+  joined_table <-
+    join_identifier(initial_table, identifier, team) %>%
+    fill(id) %>%
+    replace_na(list(current = FALSE)) %>%
+    mutate(level = ifelse(!duplicated(team) &
+                            current, "FRANCHISE", "TEAM"))
+  
+  joined_table
+}
+
+get_teams_alternative_names <- function(teams_identifier_table) {
+  teams_alts <-
+    teams_identifier_table %>%
+    group_by(id) %>%
+    summarise(alternative_names = str_c(unique(team), collapse = ", "))
+  
+  teams_alts
+}
+
+join_teams_alternative_names <- function(teams_identifier_table, teams_alts) {
+  teams_table <-
+    join_identifier(teams_identifier_table, teams_alts, id) %>%
+    filter(level == "FRANCHISE")
 }
